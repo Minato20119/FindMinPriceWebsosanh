4f76d26cb5f01385482f0cd0adb903b4ff980077
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2018-06-18 23:45:30 +0700

    Update searchPro.java

--- JavaApplication2/src/javaapplication2/searchPro.java
+++ JavaApplication2/src/javaapplication2/searchPro.java
@@ -32,7 +32,7 @@ public class searchPro {
     private static final String REGEX_GET_PRICE_IN_BLOCK = "(class=\"price \">\\s*)([0-9.]+)";
     private static final String REGEX_GET_NUMBER_PAGES = "x=\"";
     private static final String REGEX_GET_NUMBER_PAGES_LAST = "\"class";
-    
+
     // set giá nhỏ nhất để bỏ qua nó (chỉ lấy giá từ 500k trở lên)
     private static final int DEFAULT_PRICE_MIN = 500000;
     static String textArea = "";
@@ -45,21 +45,31 @@ public class searchPro {
             BufferedReader inputText = new BufferedReader(reader);
 
             String textFromFileTxt;
+            int count = 1;
 
             while ((textFromFileTxt = inputText.readLine()) != null) {
 
+                if (textFromFileTxt.equals("")) {
+                    continue;
+                }
+
                 String singleText = textFromFileTxt;
 
-                String symbolSpecial = "!@#$%^&*()_-=+{}[]|\\:;'\",<.>/?`~’–";
+                String symbolSpecial = "!@#$%^&*()_-=+{}[]|\\:;'\",<.>/?`~’";
 
-                String tempSingleText = singleText;
-
-                for (int i = 0; i < tempSingleText.length(); i++) {
+                for (int i = 0; i < textFromFileTxt.length(); i++) {
                     if (symbolSpecial.contains(singleText.substring(i, i + 1))) {
                         singleText = singleText.replace(singleText.substring(i, i + 1), " ");
                     }
                 }
+
                 singleText = singleText.replaceAll("\\s+", " ").toLowerCase();
+
+                if (singleText.contains("–")) {
+                    singleText = singleText.replaceAll("–", " ");
+                }
+
+                System.out.println(count++ + ": " + textFromFileTxt);
 
                 String textBlockContainsPrice = "";
 
@@ -69,55 +79,57 @@ public class searchPro {
                         String textLineStream;
                         String textFromStreamURL = "";
 
-                        String encodeSingleText = URLEncoder.encode(singleText, "UTF-8");
+                        String encodeSingleText = URLEncoder.encode(singleText.trim(), "UTF-8");
 
                         encodeSingleText = "https://websosanh.vn/s/" + encodeSingleText;
 
                         String urlText = encodeSingleText + "?pi=" + (String.valueOf(numberPage)) + ".htm";
-                        
-                        System.out.println(urlText);
 
                         URL url = new URL(urlText);
 
                         URLConnection connectURL = url.openConnection();
-                        
-                        
-                        // Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.225.3.1", 3128));
-			// URLConnection connectURL = new URL(urlText).openConnection(proxy);
-                        
 
+                        // Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.225.3.1", 3128));
+                        // URLConnection connectURL = new URL(urlText).openConnection(proxy);
                         try (BufferedReader inputURL = new BufferedReader(new InputStreamReader(
                                 connectURL.getInputStream()))) {
                             while ((textLineStream = inputURL.readLine()) != null) {
                                 textFromStreamURL += textLineStream + "\n";
-                                
+
                                 if (textFromStreamURL.contains("Xem thêm...")) {
                                     break;
                                 }
                             }
-                            
+
                             if (textFromStreamURL.contains("giá từ ")) {
                                 textFromStreamURL = textFromStreamURL.replaceAll("giá từ ", "");
                             }
-                            
+
                             Pattern pattern = Pattern.compile(REGEX_GET_BLOCK_CONTAINS_PRICE);
                             Matcher matcher = pattern.matcher(textFromStreamURL.toLowerCase());
-                            
+
                             while (matcher.find()) {
                                 // Het Hang
                                 if (matcher.group(0).compareTo("out-of-stock") == 0) {
                                     continue;
                                 }
-                                
-                                if (matcher.group(0).contains(singleText) || matcher.group(0).contains(textFromFileTxt.toLowerCase())) {
+
+                                if (matcher.group(0).contains(textFromFileTxt.toLowerCase().substring(0, textFromFileTxt.indexOf(" ")))) {
+                                    textBlockContainsPrice += matcher.group(0);
+                                    continue;
+                                }
+
+                                String firstText = textFromFileTxt.toLowerCase().substring(0, textFromFileTxt.indexOf(" "));
+
+                                if (textFromFileTxt.toLowerCase().contains(firstText)) {
                                     textBlockContainsPrice += matcher.group(0);
                                 }
-                                
+
                             }
-                            
+
                             Pattern pattern1 = Pattern.compile(REGEX_GET_PRICE_IN_BLOCK);
                             Matcher matcher1 = pattern1.matcher(textBlockContainsPrice);
-                            
+
                             while (matcher1.find()) {
                                 // Continute if price set = 1
                                 if (matcher1.group(2).compareTo("1") == 0) {
@@ -125,16 +137,16 @@ public class searchPro {
                                 }
                                 // Get price
                                 price = Integer.parseInt(matcher1.group(2).replace(".", ""));
-                                
+
                                 if (price <= DEFAULT_PRICE_MIN) {
                                     continue;
                                 }
-                                
+
                                 if (price < defaultPrice) {
                                     defaultPrice = price;
                                 }
                             }
-                            
+
                             // Get sumPages to get all price
                             if (sumPages == 0) {
                                 if (textFromStreamURL.lastIndexOf(REGEX_GET_NUMBER_PAGES) != -1) {
@@ -153,9 +165,8 @@ public class searchPro {
                 } while (numberPage <= sumPages);
 
                 textArea += textFromFileTxt + "\n-----------------------------------------> PriceMin: " + defaultPrice + " đ\n";
-                
-                builder.append(textArea);
 
+                builder.append(textArea);
             }
         } catch (IOException | NumberFormatException e1) {
             System.out.println("Error Input Text.");
